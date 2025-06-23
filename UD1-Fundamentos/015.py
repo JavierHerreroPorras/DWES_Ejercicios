aa916701ed24228e78a4befa2478045aa01cfc5c
Olga M. Moreno <olga3emes@gmail.com>
2025-06-23 09:48:57 +0200

    Add examples of tuples and dictionaries in Python

---
+++ UD1-Fundamentos/015.py
@@ -0,0 +1,8 @@
+#Diccionarios
+
+d = {"Love Actually ": "Richard Curtis", "Kill Bill":"Tarantino","Amélie": "Jean-Pierre Jeunet"}
+d["Love Actually "] # devuelve "Richard Curtis"
+d["Kill Bill"] = "Quentin Tarantino"
+f = { } # crear un diccionario vacio
+f["Torrente"]= "Santiago Segura" # insertar un elemento
+f["Torrente"]= "Santi Segura" # actualización del valor
