1b796b8bc5c65190d6e5da4ef2f8733439119092
Olga M. Moreno <olga3emes@gmail.com>
2025-06-23 09:37:37 +0200

    Add examples of list slicing and string manipulation in Python

---
+++ UD1-Fundamentos/010.py
@@ -0,0 +1,14 @@
+#Strings o cadenas de caracteres
+triple=""" esto es una linea
+ y esta es otra linea"""
+print(triple)  # Imprime el texto con saltos de línea
+# Concatenación de cadenas
+cadena1 = "Hola"
+cadena2 = "Mundo"
+cadena_concatenada = cadena1 + " " + cadena2
+print(cadena_concatenada)  # Imprime 'Hola Mundo'
+# Repetición de cadenas
+cadena_repetida = cadena1 * 3
+print(cadena_repetida)  # Imprime 'HolaHolaHola'
+# Longitud de una cadena
+longitud_cadena = len(cadena1)
---
+++ UD1-Fundamentos/011.py
@@ -0,0 +1,9 @@
+# Ejemplo de uso de type()
+type(3.14)  # Devuelve <class 'float'>
+type("Hola")  # Devuelve <class 'str'>
+type(True)  # Devuelve <class 'bool'>
+type(42)  # Devuelve <class 'int'>
+type([1, 2, 3])  # Devuelve <class 'list'>
+type({1, 2, 3}) # Devuelve <class 'set'>
+type({"clave": "valor"})  # Devuelve <class 'dict'>
+
